1bb1db26cfdadba34be299d97bbfd4b7c2d4539b
Claudio Carotenuto <c.carotenuto@majorbit.com>
2014-10-25 14:07:26 +0200

    Bump to version 5.0.2

--- lib/jquery/ui/sass/rails/version.rb
+++ lib/jquery/ui/sass/rails/version.rb
@@ -2,8 +2,8 @@ module Jquery
   module Ui
     module Sass
       module Rails
-        VERSION = "4.0.3.0"
-        JQUERY_UI_RAILS_VERSION = "4.0.3"
+        VERSION = "5.0.2.0"
+        JQUERY_UI_RAILS_VERSION = "5.0.2"
       end
     end
   end
